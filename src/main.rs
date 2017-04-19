92c922b8ec60e215d403f2872f34ef0a5ec5f4db
tasgoon <tasgo@outlook.com>
2017-04-18 21:56:59 -0400

    still learning rust, stuff not working

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
-extern crate rusqlite;
+//extern crate rusqlite;
 
-use rusqlite::Connection;
+//use rusqlite::Connection;
 use std::fs::File;
 use std::thread::sleep;
 use std::time::Duration;
 
 mod recorder;
 
-static INPUT: &'static str = "/sensors/acceleromoter/data";
-static OUTPUT: &'static str = "data.db";
+static INPUT: &'static str = "/sensors/accelerometer/data";
+//static OUTPUT: &'static str = "data.db";
 
 fn main() {
-    let db = Connection::open(OUTPUT).unwrap();
-    db.execute("CREATE TABLE data (
+    /*let db = Connection::open(OUTPUT).unwrap();
+    let create_result = db.execute("CREATE TABLE data (
             ax      INTEGER NOT NULL,
             ay      INTEGER NOT NULL,
             az      INTEGER NOT NULL,
-            time    TEXT NOT NULL", &[]).unwrap();
+            time    TEXT NOT NULL", &[]);*/
     let sleep_time = Duration::from_millis(100);
     loop {
-        let mut file = File::create(INPUT).unwrap();
-        let acc = recorder::read_acceleration(&mut file);
-        db.execute("INSERT INTO data (ax, ay, az, time)
-            VALUES (?1, ?2, ?3, strftime('%f', 'now')", &[&acc.ax, &acc.ay, &acc.az]).unwrap();
+        let file = File::open(INPUT).unwrap();
+        let acc = recorder::read_acceleration(file);
+        println!("ax: {}, ay: {}, az: {}", acc.ax, acc.ay, acc.az);
+        //db.execute("INSERT INTO data (ax, ay, az, time)
+        //    VALUES (?1, ?2, ?3, strftime('%f', 'now')", &[&acc.ax, &acc.ay, &acc.az]).unwrap();
         sleep(sleep_time);
     }
 }
--- src/recorder.rs
+++ src/recorder.rs
@@ -7,9 +7,20 @@ pub struct Acceleration {
     pub az: i32
 }
 
-pub fn read_acceleration(file: &mut File) -> Acceleration {
+pub fn read_acceleration(mut file: File) -> Acceleration {
+
     let mut data = String::new();
-    file.read_to_string(&mut data).unwrap();
-    let input: Vec<i32> = data.split(",").map(|x| x.parse::<i32>().unwrap()).collect();
-    return Acceleration { ax: input[0], ay: input[1], az: input [2] };
+    let result = file.read_to_string(&mut data);
+    if result.is_ok() {
+        result.unwrap();
+        println!("{}", data);
+        let split: Vec<&str> = data.split(",").collect();
+        println!("{}", split[0]);
+        //let input: Vec<i32> = split.map(|x| x.parse::<i32>().unwrap_or(0)).collect();
+        //return Acceleration { ax: input[0], ay: input[1], az: input[2] };
+        return Acceleration { ax: 0, ay: 0, az: 0};
+    } else {
+        println!("Error reading input.");
+        return Acceleration { ax: 0, ay: 0, az: 0};
+    }
 }
